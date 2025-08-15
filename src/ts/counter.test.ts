a3230d22a1877861170ca363481fd7517122ce2c
zkfrov <frov@wonderland.xyz>
2025-08-15 18:10:44 +0200

    feat: address derivation + secret to public keys

--- src/ts/counter.test.ts
+++ src/ts/counter.test.ts
@@ -7,10 +7,17 @@ import {
   CompleteAddress,
   PXE,
   AccountWalletWithSecretKey,
+  Fr,
 } from "@aztec/aztec.js";
+import {
+  deployCounterWithPublicKeysAndSalt,
+  setupSandbox,
+  predictContractAddress
+} from "./utils.js";
 import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
-import { deployCounter, setupSandbox } from "./utils.js";
 import { describe, it, expect, beforeAll, beforeEach } from "vitest";
+import { deriveKeys } from '@aztec/stdlib/keys';
+import { count } from "console";
 
 describe("Counter Contract", () => {
   let pxe: PXE;
@@ -22,6 +29,8 @@ describe("Counter Contract", () => {
   let carl: AccountWallet;
 
   let counter: CounterContract;
+  let keys: any;
+  let salt: Fr;
 
   beforeAll(async () => {
     pxe = await setupSandbox();
@@ -32,20 +41,54 @@ describe("Counter Contract", () => {
     alice = wallets[0];
     bob = wallets[1];
     carl = wallets[2];
+
+    // Keys and salt are used to deploy and derive the contract address
+    // Derive keys for the contract with deterministic value
+    keys = await deriveKeys(new Fr(1));
+    console.log(keys);
   });
 
   beforeEach(async () => {
-    counter = await deployCounter(alice, alice.getAddress());
+    // Using a random salt for each test run to avoid Existing nullifier error
+    salt = Fr.random();
+
+    // Deploy the Counter contract with the public keys
+    counter = await deployCounterWithPublicKeysAndSalt(
+      keys.publicKeys,
+      alice,
+      alice.getAddress(),
+      salt
+    );
   });
 
-  it("e2e", async () => {
-    const owner = await counter.methods.get_owner().simulate();
-    expect(owner).toStrictEqual(alice.getAddress());
-    // default counter's value is 0
-    expect(await counter.methods.get_counter().simulate()).toBe(0n);
-    // call to `increment`
-    await counter.methods.increment().send().wait();
-    // now the counter should be incremented.
-    expect(await counter.methods.get_counter().simulate()).toBe(1n);
+  it("Deploy and predicted address match", async () => {
+    const { address } = await predictContractAddress(
+      CounterContractArtifact,
+      [alice.getAddress()],
+      alice.getAddress(),
+      salt,
+      keys.publicKeys,
+    );
+
+    console.log("Deployed address:  ", counter.address.toString());
+    console.log("Predicted address: ", address.toString());
+
+    expect(counter.address.toString()).toBe(address.toString());
+  });
+
+  it("Computing masterNullifierPublicKey should match", async () => {
+    const output = await counter.methods.private_to_public_keys(new Fr(keys.masterNullifierSecretKey)).simulate();
+    
+    // Master Nullifier Public Key X and Y
+    const mnpkx = new Fr(output.x);
+    const mnpky = new Fr(output.y);
+
+    console.log("Master Nullifier Public Key X: ", mnpkx);
+    console.log("Public Keys X:                 ", new Fr(keys.publicKeys.masterNullifierPublicKey.x));
+    console.log("Master Nullifier Public Key Y: ", mnpky);
+    console.log("Public Keys Y:                 ", keys.publicKeys.masterNullifierPublicKey.y);
+
+    expect(mnpkx.toString()).toBe(keys.publicKeys.masterNullifierPublicKey.x.toString());
+    expect(mnpky.toString()).toBe(keys.publicKeys.masterNullifierPublicKey.y.toString());
   });
 });
--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -9,6 +9,17 @@ import {
   CounterContract,
   CounterContractArtifact,
 } from "../artifacts/Counter.js";
+import {
+  Fr,
+  getContractClassFromArtifact,
+  PublicKeys
+} from '@aztec/aztec.js';
+import { getDefaultInitializer } from '@aztec/stdlib/abi';
+import {
+  computeInitializationHash,
+  computeContractAddressFromInstance,
+  computeSaltedInitializationHash
+} from '@aztec/stdlib/contract';
 
 export const createPXE = async (id: number = 0) => {
   const { BASE_PXE_URL = `http://localhost` } = process.env;
@@ -41,4 +52,68 @@ export async function deployCounter(
     .send()
     .deployed();
   return contract as CounterContract;
+}
+
+/**
+ * Deploys the Counter contract.
+ * @param publicKeys - The public keys to use for the contract.
+ * @param deployer - The wallet to deploy the contract with.
+ * @param owner - The address of the owner of the contract.
+ * @param salt - The salt to use for the contract address. If not provided, a random salt will be used.
+ * @returns A deployed contract instance.
+ */
+export async function deployCounterWithPublicKeysAndSalt(
+  publicKeys: PublicKeys,
+  deployer: AccountWallet,
+  owner: AztecAddress,
+  salt: Fr = Fr.random(),
+): Promise<CounterContract> {
+  const contract = await Contract.deployWithPublicKeys(
+    publicKeys,
+    deployer,
+    CounterContractArtifact,
+    [owner],
+    "constructor",
+  )
+    .send({ contractAddressSalt: salt })
+    .deployed();
+  return contract as CounterContract;
+}
+
+/**
+ * Predicts the contract address for a given artifact and constructor arguments.
+ * @param artifact - The contract artifact.
+ * @param constructorArgs - The arguments to pass to the constructor.
+ * @param deployer - The address of the deployer.
+ * @param salt - The salt to use for the contract address. If not provided, a random salt will be used.
+ * @param publicKeys - The public keys to use for the contract.
+ * @returns The predicted contract address.
+ */
+export async function predictContractAddress(
+  artifact: any,
+  constructorArgs: any,
+  deployer: AztecAddress = AztecAddress.ZERO,
+  salt: Fr = Fr.random(),
+  publicKeys: PublicKeys,
+) {
+  if (!publicKeys) {
+    publicKeys = await PublicKeys.random();
+  }
+
+  const contractClass = await getContractClassFromArtifact(artifact);
+  const constructorArtifact = getDefaultInitializer(artifact);
+  const initializationHash = await computeInitializationHash(constructorArtifact, constructorArgs);
+  const saltedInitializationHash = await computeSaltedInitializationHash({
+    initializationHash,
+    salt,
+    deployer,
+  });
+
+  const address = await computeContractAddressFromInstance({
+    originalContractClassId: contractClass.id,
+    saltedInitializationHash: saltedInitializationHash,
+    publicKeys: publicKeys
+  });
+
+  return { address, initializationHash };
 }
